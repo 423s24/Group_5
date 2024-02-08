1568dec609be47a62df7608e03a0361422d673a6
MikeKadoshnikov <mikekadoshnikov@gmail.com>
2024-02-08 13:54:31 -0700

    updated the forms
    put maintenance in its own folder and application in its own folder

--- HousingPortal/Portal/urls.py
+++ HousingPortal/Portal/urls.py
@@ -4,6 +4,6 @@ from .views import index, application, maintenance
 
 urlpatterns = [
     path('', index, name='index'),
-    path('forms/', application, name='application'),
-    path('forms/', maintenance, name='maintenance'),
+    path('forms/application', application, name='application'),
+    path('forms/maintenance', maintenance, name='maintenance'),
 ]
--- HousingPortal/Portal/views.py
+++ HousingPortal/Portal/views.py
@@ -6,10 +6,10 @@ def index(request):
     return render(request, 'index.html')
 
 def application(request):
-    return render(request, 'forms/application.html')
+    return render(request, 'forms/application/application.html')
 
 def maintenance(request):
-    return render(request, 'forms/maintenance.html')
+    return render(request, 'forms/maintenance/maintenance.html')
 
 def user_profile(request, username):
     return render(request, 'profile.html', {'user_profile' : user_profile})
