30c6d1021eba5ef7fef602cefd9ae43a910284fb
jtann5 <35159340+jtann5@users.noreply.github.com>
2024-03-26 14:34:10 -0600

    Fix test imports

--- HousingPortal/Portal/tests.py
+++ HousingPortal/Portal/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
-import HousingPortal.Portal
-from HousingPortal.Portal.models import UserAccount, MaintenanceNotes, MaintenanceRequest
+from .models import *
 
 # Create your tests here.
 
